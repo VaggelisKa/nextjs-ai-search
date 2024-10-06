3c9a92285a94ac85f9dbf003e9c672dc0acce535
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2024-10-06 09:25:58 +0200

    Bootstrapping search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
-import { db } from "~/lib/db/db";
-import { documents } from "~/lib/db/schema";
-
 export default async function Home() {
-  const data = await db.select().from(documents);
+  async function searchAction(formData: FormData) {
+    "use server";
 
-  console.log("testing 123", data);
+    console.log(formData.get("search"));
+  }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-xl">
-        Nextjs search with text embeddings
+        <span>Nextjs search with text embeddings</span>
+
+        <form action={searchAction} className="flex flex-col gap-4 w-full">
+          <input
+            autoFocus
+            className="p-4 text-black outline-blue-400 rounded-sm outline-offset-2"
+            type="text"
+            placeholder="Search"
+            name="search"
+          />
+
+          <button
+            type="submit"
+            className="p-4 bg-blue-500 text-white rounded-sm font-semibold"
+          >
+            Search
+          </button>
+        </form>
       </main>
     </div>
   );
