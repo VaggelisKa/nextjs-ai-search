9dca0d730392ea7b704ccf0956a7e3cab9e7c4d4
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2024-10-06 09:17:06 +0200

    Use pgVector for column type

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,9 +1,9 @@
 import {
-  doublePrecision,
   pgTableCreator,
   serial,
   text,
   timestamp,
+  vector,
 } from "drizzle-orm/pg-core";
 
 const pgTable = pgTableCreator((name) => `nextAiSearch_${name}`);
@@ -11,6 +11,6 @@ const pgTable = pgTableCreator((name) => `nextAiSearch_${name}`);
 export const documents = pgTable("documents", {
   id: serial("id").primaryKey(),
   content: text("content"),
-  embedding: doublePrecision("embedding"),
+  embedding: vector("embedding", { dimensions: 3 }),
   createdAt: timestamp("created_at").defaultNow(),
 });
